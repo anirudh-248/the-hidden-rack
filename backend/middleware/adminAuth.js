471b8fc0ac6b28f1befbb3fde331872af1aba925
anirudh-248 <anirudhphaniraj1@gmail.com>
2025-06-18 17:58:04 +0530

    fix adminAuth function

--- backend/middleware/adminAuth.js
+++ backend/middleware/adminAuth.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 const adminAuth = (req, res, next) => {
   try {
-    const { token } = req.headers;
+    const { token } = req.headers.token;
     if (!token) {
       return res.json({
         success: false,
