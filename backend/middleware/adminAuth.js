11117f02791ccb831374990548a897a2d05704b7
anirudh-248 <anirudhphaniraj1@gmail.com>
2025-06-18 18:03:04 +0530

    revert adminAuth function to original

--- backend/middleware/adminAuth.js
+++ backend/middleware/adminAuth.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 const adminAuth = (req, res, next) => {
   try {
-    const { token } = req.headers.token;
+    const { token } = req.headers;
     if (!token) {
       return res.json({
         success: false,
